2bd5de9cb0dfe70bcf282b92206cc76258318271
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-03-20 01:50:19 +0300

    arrowlab complete

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function (a, b) {
+  a = 2000;
+  b = 100;
+  return a / b;
+};
+divide(a, b);
+
+const square = (x) => x * x;
+let x = 2;
+square(x);
+
+const add = (a, b) => a + b
+let a = 3;
+let b = 4;
+add(a,b)
